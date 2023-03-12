bbd2d82e323611243d9f78511182f4ea9106daf1
Jeremy Little <muqluk@yahoo.com>
2023-03-12 14:50:07 -0500

    added introduction to winforms in .Net 7 project

---
+++ src/dotnet 7/winforms/examples/IntroToWinforms/MainForm.cs
@@ -0,0 +1,44 @@
+﻿namespace example_winforms_app {
+  public partial class MainForm : Form {
+
+    #region CTOR
+
+    public MainForm() => InitializeComponent();
+
+    #endregion
+
+    #region Event Handlers
+
+    private void btn_addPerson_Click(object sender, EventArgs e) => AddPerson();
+
+    private void cmb_SelectionChangeEvent(object sender, EventArgs e) => SetSelectedPerson();
+
+    private void cmb_RemovePerson_Click(object sender, EventArgs e) => RemovePerson();
+
+    #endregion
+
+    #region Private Methods
+
+    private void AddPerson() {
+      cmb_names.Items.Add($"{txt_firstName.Text.Trim()} {txt_lastName.Text.Trim()}");
+      ClearUserInput();
+    }
+
+    private void ClearUserInput() {
+      txt_firstName.Clear();
+      txt_lastName.Clear();
+    }
+
+    private void SetSelectedPerson() {
+      txt_firstName.Text = cmb_names.SelectedItem.ToString()?.Split(' ')[0] ?? String.Empty;
+      txt_lastName.Text = cmb_names.SelectedItem.ToString()?.Split(' ')[1] ?? String.Empty;
+    }
+
+    private void RemovePerson() {
+      cmb_names.Items.RemoveAt(cmb_names.SelectedIndex);
+      ClearUserInput();
+    }
+
+    #endregion
+  }
+}
---
+++ src/dotnet 7/winforms/examples/IntroToWinforms/readme.md
@@ -0,0 +1,3 @@
+# Introduction to WinForms in .Net 7<sub><sup><sup>_[~ by Jeremy Little](mailto:jeremy@jwlittle.net?subject=Refresher-Projects%20repo,%20how-to-fork-a-repo.md%20page)_</sup></sup></sup><sup><br /> - _New form creation_</sup>
+
+> Code walkthrough coming Soon
